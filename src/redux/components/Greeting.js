969d587751a039f558e98bccb84882ea9d6d9a75
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-12-14 18:25:10 -0600

    fix linters

--- src/redux/components/Greeting.js
+++ src/redux/components/Greeting.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import fetchRandomGreeting from '../actions/greetingActions';
+import { fetchRandomGreeting } from '../actions/greetingActions';
 
 const Greeting = () => {
   const dispatch = useDispatch();
